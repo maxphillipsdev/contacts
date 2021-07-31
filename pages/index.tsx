cd6fda8ef3ddc3e0801f6c88dfcc8b8c620e0691
Max Phillips <max@maxphillipsdev.com>
2021-07-31 13:07:11 +1000

    Added modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,7 @@ import AddContactForm from "../components/shell/AddContactForm";
 const Home: React.FC = () => {
   const toast = useToast();
 
-  return (
-    <Flex h="100%" justifyContent="center" alignItems="center">
-      <AddContactForm />
-      {/* <Button
-        onClick={() => {
-          toast({
-            title: "Clicked!",
-            description: "nice.",
-            status: "success",
-          });
-        }}
-      >
-        Click me!
-      </Button> */}
-    </Flex>
-  );
+  return <Flex h="100%" justifyContent="center" alignItems="center"></Flex>;
 };
 
 export default Home;
