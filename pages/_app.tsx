913a864786e6fb4788429a261bf7d9f1b923116a
Max Phillips <max@maxphillipsdev.com>
2021-08-13 23:08:22 +1000

    Add sentry init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,15 @@ import AppShell from "../components/shell/AppShell";
 import DBContext, { ContactsDB } from "../lib/db";
 import theme from "../theme";
 
+Sentry.init({
+  dsn: "https://1f84883d6be1447c886f88fe70ff7de0@o302306.ingest.sentry.io/5905560",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [db, setDb] = useState<ContactsDB | null>(null);
 
